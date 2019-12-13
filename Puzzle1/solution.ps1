07e6f67b2ea0533b699a991af31425d66bff6f7e
Neil Barnwell <neil.barnwell@bybox.com>
2019-12-13 18:38:08 +0000

    Day 1, Part 2

--- Puzzle1/solution.ps1
+++ Puzzle1/solution.ps1
@@ -1,5 +1,31 @@
+
+function CalculateFuel {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [int[]] $Mass
+    )
+    process {
+        $Mass |
+            ForEach-Object{ $_ / 3 } | 
+            ForEach-Object{ [Math]::Floor($_) } | 
+            ForEach-Object{ $_ - 2 } | 
+            ForEach-Object { [Math]::Max($_, 0) } |
+            Measure-Object -Sum |
+            Select-Object -exp Sum
+    }
+}
+
 Get-Content (Join-Path $PSScriptRoot 'input.txt') | 
-    %{ $_ / 3 } | 
-    %{ [Math]::Floor($_) } | 
-    %{ $_ - 2 } | 
-    measure -Sum
+    CalculateFuel |
+    ForEach-Object{
+        $moduleFuel = $_
+        $additionalFuel = $moduleFuel
+        do {
+            $additionalFuel = CalculateFuel $additionalFuel
+            $moduleFuel += $additionalFuel
+        } while ($additionalFuel -gt 0)
+
+        $moduleFuel
+    } |
+    Measure-Object -Sum
