cfff42c5f73184baf7fad08bf457f6921ee5171f
Neil Barnwell <neil.barnwell@bybox.com>
2019-12-30 00:07:19 +0000

    Puzzle4 part 2 solved

--- Puzzle4/solution.ps1
+++ Puzzle4/solution.ps1
@@ -1,32 +1,15 @@
-<#
-307237..769058 |
-    Where-HasLength 6 |
-    Where-Object {
-        # Two adjacent digits are equal
-        $found = $false
-        $stringArray = $_.ToString()
-        for ($i = 0; $i -lt $stringArray.Length - 1; $i++) {
-            if ($stringArray[$i + 1] -eq $stringArray[$i]) {
-                $found = $true
-                break
-            }
+
+filter Where-DigitsNeverDecrease {
+    # All digits are greater than or equal to the last
+    $stringArray = $_.ToString()
+    for ($i = 0; $i -lt $stringArray.Length - 1; $i++) {
+        if ([int]$stringArray[$i + 1] -lt [int]$stringArray[$i]) {
+            return $null
         }
-        $found
-    } |
-    Where-Object {
-        # All digits are greater than or equal to the last
-        $correct = $true
-        $stringArray = $_.ToString()
-        for ($i = 0; $i -lt $stringArray.Length - 1; $i++) {
-            if ([int]$stringArray[$i + 1] -lt [int]$stringArray[$i]) {
-                $correct = $false
-                break
-            }
-        }
-        $correct
-    } |
-    Measure-Object
-    #>
+    }
+    $_
+}
+
 filter Where-HasLength {
     param(
         [int] $Length
@@ -38,14 +21,32 @@ filter Where-HasLength {
 
 filter Where-ContainsDouble {
     # Two adjacent digits are equal
-    $found = $false
     $stringArray = $_.ToString()
-    for ($i = 0; $i -lt $stringArray.Length - 1; $i++) {
-        if ($stringArray[$i + 1] -eq $stringArray[$i]) {
-            return $_
+    [char] $prev = $null
+    [int] $counter = 0
+    for ($i = 0; $i -lt $stringArray.Length; $i++) {
+        $cur = $stringArray[$i]
+        if ($cur -eq $prev) {
+            $counter++
+        } else {
+            if ($counter -eq 2) {
+                return $_
+            }
+            $counter = 1
         }
+        $prev = $cur
+    }
+
+    if ($counter -eq 2) {
+        return $_
     }
 }
+
+307237..769058 |
+    Where-HasLength 6 |
+    Where-ContainsDouble |
+    Where-DigitsNeverDecrease |
+    Measure-Object
 
 Describe 'Matching number rules' {
     Context 'PIN length' {
@@ -63,6 +64,16 @@ Describe 'Matching number rules' {
             1223 | Where-ContainsDouble | Should Be 1223
             1233 | Where-ContainsDouble | Should Be 1233
             1234 | Where-ContainsDouble | Should Be $null
+            12224 | Where-ContainsDouble | Should Be $null
+            111122 | Where-ContainsDouble | Should Be 111122
+        }
+    }
+
+    Context 'Sequential digits never decrease' {
+        It 'must only contain digits that are equal to or greater than the previous' {
+            123 | Where-DigitsNeverDecrease | Should Be 123
+            122 | Where-DigitsNeverDecrease | Should Be 122
+            121 | Where-DigitsNeverDecrease | Should Be $null
         }
     }
 }
